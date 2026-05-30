f97ea1c30deb7b753dffd05015f4e980e757f819
strivexxofficial-jpg <strivexxofficial@gmail.com>
2026-05-30 18:37:15 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json({ limit: "50mb" }));
 
