29032b6d53762d620e5a74d1a45ccf5f30721298
strivexxofficial-jpg <strivexxofficial@gmail.com>
2026-05-30 17:00:40 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -149,10 +149,9 @@ async function startServer() {
 
     app.use(express.static(distPath));
 
- app.get("/(.*)", (req: any, res: any) => {
-      res.sendFile(path.join(distPath, "index.html"));
-    });
-  }
+app.get("*", (req: any, res: any) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
